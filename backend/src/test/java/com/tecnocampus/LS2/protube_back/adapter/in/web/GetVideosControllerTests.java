45a444c13ab6ac2ddca855ec62b7232848a08645
Aitor Juanola <119131537+ajutra@users.noreply.github.com>
2024-10-07 23:38:34 +0200

    Fix typo on class name

--- backend/src/test/java/com/tecnocampus/LS2/protube_back/adapter/in/web/GetVideosControllerTests.java
+++ backend/src/test/java/com/tecnocampus/LS2/protube_back/adapter/in/web/GetVideosControllerTests.java
@@ -18,7 +18,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-public class GetVideosControllerTest {
+public class GetVideosControllerTests {
 
     private MockMvc mockMvc;
 
