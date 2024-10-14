93e6c5c4e7888c87da3c13e80255f5c87e2039bb
Aitor Juanola <119131537+ajutra@users.noreply.github.com>
2024-10-14 15:18:35 +0200

    Implement tag domain model, mapper and tests

---
+++ backend/src/main/java/com/tecnocampus/LS2/protube_back/adapter/out/persistence/mapper/TagMapper.java
@@ -0,0 +1,14 @@
+package com.tecnocampus.LS2.protube_back.adapter.out.persistence.mapper;
+
+import com.tecnocampus.LS2.protube_back.adapter.out.persistence.jpaEntity.TagJpaEntity;
+import com.tecnocampus.LS2.protube_back.domain.model.Tag;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TagMapper {
+    public Tag toDomain(TagJpaEntity tagJpaEntity) {
+        return new Tag(
+                tagJpaEntity.getTag_name()
+        );
+    }
+}
---
+++ backend/src/test/java/com/tecnocampus/LS2/protube_back/adapter/out/persistence/mapper/TagMapperTests.java
@@ -0,0 +1,20 @@
+package com.tecnocampus.LS2.protube_back.adapter.out.persistence.mapper;
+
+import com.tecnocampus.LS2.protube_back.adapter.out.persistence.jpaEntity.TagJpaEntity;
+import com.tecnocampus.LS2.protube_back.domain.model.Tag;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TagMapperTests {
+
+    private final TagMapper tagMapper = new TagMapper();
+
+    @Test
+    void mapTagJpaEntityToDomain() {
+        TagJpaEntity tagJpaEntity = new TagJpaEntity("tag_name1");
+
+        Tag tag = tagMapper.toDomain(tagJpaEntity);
+
+        assertEquals("tag_name1", tag.name());
+    }
+}
