9bd42712907376eda7885b97397834514f8d7db5
Aitor Juanola <119131537+ajutra@users.noreply.github.com>
2024-10-14 14:56:52 +0200

    Implement User domain model and User mapper

---
+++ backend/src/main/java/com/tecnocampus/LS2/protube_back/adapter/out/persistence/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.tecnocampus.LS2.protube_back.adapter.out.persistence.mapper;
+
+import com.tecnocampus.LS2.protube_back.adapter.out.persistence.jpaEntity.UserJpaEntity;
+import com.tecnocampus.LS2.protube_back.domain.model.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+    public User toDomain(UserJpaEntity userJpaEntity) {
+        return new User(
+                userJpaEntity.getUser_id(),
+                userJpaEntity.getUsername()
+        );
+    }
+}
---
+++ backend/src/main/java/com/tecnocampus/LS2/protube_back/domain/model/User.java
@@ -0,0 +1,7 @@
+package com.tecnocampus.LS2.protube_back.domain.model;
+
+public record User(
+        String id,
+        String username
+) {
+}
