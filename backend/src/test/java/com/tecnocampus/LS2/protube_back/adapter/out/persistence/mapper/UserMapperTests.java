23f34a74d1f9aee7876661a43d5c5351eaf8f6d0
Aitor Juanola <119131537+ajutra@users.noreply.github.com>
2024-10-14 14:56:29 +0200

    Implement User mapper tests

---
+++ backend/src/test/java/com/tecnocampus/LS2/protube_back/adapter/out/persistence/mapper/UserMapperTests.java
@@ -0,0 +1,22 @@
+package com.tecnocampus.LS2.protube_back.adapter.out.persistence.mapper;
+
+import com.tecnocampus.LS2.protube_back.adapter.out.persistence.jpaEntity.UserJpaEntity;
+import com.tecnocampus.LS2.protube_back.domain.model.User;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserMapperTests {
+
+    private final UserMapper userMapper = new UserMapper();
+
+    @Test
+    void mapUserJpaEntityToDomain() {
+        UserJpaEntity userJpaEntity = new UserJpaEntity("user_id1", "username1");
+
+        User user = userMapper.toDomain(userJpaEntity);
+
+        assertEquals("user_id1", user.id());
+        assertEquals("username1", user.username());
+    }
+}
