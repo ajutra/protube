18713c5cc7a73a9a7e5a7884ab9b72fe36307109
Aitor Juanola <119131537+ajutra@users.noreply.github.com>
2024-10-26 13:01:17 +0200

    Remove unused imports

--- backend/src/main/java/com/tecnocampus/LS2/protube_back/adapter/in/web/GetVideoController.java
+++ backend/src/main/java/com/tecnocampus/LS2/protube_back/adapter/in/web/GetVideoController.java
@@ -1,10 +1,8 @@
 package com.tecnocampus.LS2.protube_back.adapter.in.web;
 
-import com.tecnocampus.LS2.protube_back.domain.model.Video;
 import com.tecnocampus.LS2.protube_back.port.in.command.GetVideoCommand;
 import com.tecnocampus.LS2.protube_back.port.in.useCase.GetAllVideosUseCase;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- backend/src/main/java/com/tecnocampus/LS2/protube_back/port/in/command/GetTagCommand.java
+++ backend/src/main/java/com/tecnocampus/LS2/protube_back/port/in/command/GetTagCommand.java
@@ -2,8 +2,6 @@ package com.tecnocampus.LS2.protube_back.port.in.command;
 
 import com.tecnocampus.LS2.protube_back.domain.model.Tag;
 
-import java.util.List;
-
 public record GetTagCommand(
         String tagName
 ) {
