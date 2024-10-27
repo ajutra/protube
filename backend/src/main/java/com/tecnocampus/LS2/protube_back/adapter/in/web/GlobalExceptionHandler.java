f35ffd8b9419d1bcc0a6671b37c5351ccde9003b
Aitor Juanola <119131537+ajutra@users.noreply.github.com>
2024-10-27 19:49:37 +0100

    Remove unused imports

--- backend/src/main/java/com/tecnocampus/LS2/protube_back/adapter/in/web/GlobalExceptionHandler.java
+++ backend/src/main/java/com/tecnocampus/LS2/protube_back/adapter/in/web/GlobalExceptionHandler.java
@@ -1,9 +1,7 @@
 package com.tecnocampus.LS2.protube_back.adapter.in.web;
 
-import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
