ab3be3c4658ee641cf3ce01a19876bc3a4b6d852
Aitor Juanola <119131537+ajutra@users.noreply.github.com>
2024-10-30 11:02:24 +0100

    Add testing coverage to project

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -12,7 +12,15 @@ const config: Config = {
   moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
     "\\.(gif|ttf|eot|svg)$": "jest-transform-stub"
-  }
+  },
+coverageThreshold: {
+global: {
+branches: 75,
+functions: 75,
+lines: 75,
+statements: 75,
+},
+},
 };
 
 export default config;
