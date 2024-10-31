086e419228095a510784ccd56d90237badb5933c
Aitor Juanola <119131537+ajutra@users.noreply.github.com>
2024-10-31 12:29:31 +0100

    Refactor category controllers and its tests

--- backend/src/main/java/com/tecnocampus/LS2/protube_back/adapter/in/web/CategoryRestController.java
+++ backend/src/main/java/com/tecnocampus/LS2/protube_back/adapter/in/web/CategoryRestController.java
@@ -1,24 +1,27 @@
 package com.tecnocampus.LS2.protube_back.adapter.in.web;
 
 import com.tecnocampus.LS2.protube_back.port.in.command.GetCategoryCommand;
+import com.tecnocampus.LS2.protube_back.port.in.command.StoreCategoryCommand;
 import com.tecnocampus.LS2.protube_back.port.in.useCase.GetAllCategoriesUseCase;
 import com.tecnocampus.LS2.protube_back.port.in.useCase.GetCategoryUseCase;
+import com.tecnocampus.LS2.protube_back.port.in.useCase.StoreCategoryUseCase;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
-public class GetCategoryController {
+public class CategoryRestController {
     private final GetAllCategoriesUseCase getAllCategoriesUseCase;
     private final GetCategoryUseCase getCategoryUseCase;
+    private final StoreCategoryUseCase storeCategoryUseCase;
 
     @GetMapping("/categories")
     public List<GetCategoryCommand> getAllCategories() {
@@ -28,5 +31,11 @@ public class GetCategoryController {
     @GetMapping("/categories/{categoryName}")
     public GetCategoryCommand getCategory(@Valid @NotBlank @PathVariable String categoryName) {
         return getCategoryUseCase.getCategory(categoryName);
+    }
+
+    @PostMapping("/categories")
+    public ResponseEntity<Void> storeCategory(@Valid @RequestBody StoreCategoryCommand storeCategoryCommand) {
+        storeCategoryUseCase.storeCategory(storeCategoryCommand);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
