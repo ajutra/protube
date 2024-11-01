d57daa7835d9fd75a88af34e39a743ace9939d00
nil roset <105341833+nilroset@users.noreply.github.com>
2024-11-01 19:01:26 +0100

    implement to show a VideoCard component to App #59

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,57 +1,39 @@
-import { useState } from 'react';
-import logo from './assets/logo.svg';
+// App.tsx
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import axios from 'axios';
+import './components/styles/VideoCard.css'
+import VideoCard from './components/VideoCard';
+
+interface Video {
+  videoFileName: string;
+  thumbnailFileName: string;
+  title: string;
+  username: string;
+}
 
 function App() {
-  const [data, setData] = useState<any[] | null>(null);
-  const [loading, setLoading] = useState<boolean>(false);
+  const [videos, setVideos] = useState<Video[]>([]);
 
-  const getVideoData = async () => {
-    setLoading(true);
-    try {
-      const response = await axios.get('http://localhost:8080/videos');
-      setData(response.data);
-    } catch (error) {
-      console.error('Error fetching data', error);
-    } finally {
-      setLoading(false);
-    }
-  };
+  useEffect(() => {
+    fetch('http://localhost:8080/api/videos')
+      .then(response => response.json())
+      .then(data => setVideos(data))
+      .catch(error => console.error("Error fetching videos: ", error));
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="App-link"
-          href="#"
-          onClick={getVideoData}
-        >
-          API Call
-        </a>
-        {loading && <p>Loading...</p>}
-        {data && (
-        <div>
-          <h2>Data from API:</h2>
-            {data.map((item, index) => (
-              <li key={index}>
-                {JSON.stringify(item)}
-              </li>
-            ))}
-        </div>
-      )}
+        {videos.length > 0 ? (
+          <VideoCard
+            videoFileName={videos[0].videoFileName}
+            thumbnailFileName={videos[0].thumbnailFileName}
+            title={videos[0].title}
+            username={videos[0].username}
+          />
+        ) : (
+          <p>Loading...</p>
+        )}
       </header>
     </div>
   );
