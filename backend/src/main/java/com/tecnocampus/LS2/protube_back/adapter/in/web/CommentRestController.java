9d1f9000bb333a8b6a5e99986d2955f94666a61f
laura <lauraparracosta11@gmail.com>
2024-11-03 12:43:25 +0100

    Improve CommentRestController #53

--- backend/src/main/java/com/tecnocampus/LS2/protube_back/adapter/in/web/CommentRestController.java
+++ backend/src/main/java/com/tecnocampus/LS2/protube_back/adapter/in/web/CommentRestController.java
@@ -4,6 +4,7 @@ import com.tecnocampus.LS2.protube_back.port.in.command.GetCommentCommand;
 import com.tecnocampus.LS2.protube_back.port.in.command.StoreCommentCommand;
 import com.tecnocampus.LS2.protube_back.port.in.useCase.GetCommentsByUsernameUseCase;
 import com.tecnocampus.LS2.protube_back.port.in.useCase.StoreCommentUseCase;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,7 +20,7 @@ public class CommentRestController {
 
     private final StoreCommentUseCase storeCommentUseCase;
 
-    private final GetCommentsByUsernameUseCase getAllCommentsUseCase;
+    private final GetCommentsByUsernameUseCase getCommentsByUsernameUseCase;
 
     @PostMapping
     public ResponseEntity<Void> storeComment(@RequestBody StoreCommentCommand storeCommentCommand) {
@@ -28,8 +29,8 @@ public class CommentRestController {
     }
 
     @GetMapping("/user/{username}")
-    public List<GetCommentCommand> getCommentsByUsername(@PathVariable @NotBlank String username) {
-        return  getAllCommentsUseCase.getCommentsByUsername(username);
+    public List<GetCommentCommand> getCommentsByUsername(@Valid @PathVariable @NotBlank String username) {
+        return  getCommentsByUsernameUseCase.getCommentsByUsername(username);
 
     }
 }
