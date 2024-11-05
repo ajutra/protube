062b747b0f671879d4978aeeb83c1a80ded754e1
laura <lauraparracosta11@gmail.com>
2024-11-05 10:50:17 +0100

    Remove unnecessary import

--- backend/src/main/java/com/tecnocampus/LS2/protube_back/domain/service/GetCommentsByVideoService.java
+++ backend/src/main/java/com/tecnocampus/LS2/protube_back/domain/service/GetCommentsByVideoService.java
@@ -1,6 +1,5 @@
 package com.tecnocampus.LS2.protube_back.domain.service;
 
-import com.tecnocampus.LS2.protube_back.domain.model.Video;
 import com.tecnocampus.LS2.protube_back.port.in.command.GetCommentCommand;
 import com.tecnocampus.LS2.protube_back.port.in.useCase.GetAllCommentsByVideoUseCase;
 import com.tecnocampus.LS2.protube_back.port.out.GetCommentPort;
