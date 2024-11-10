e8719d4e03ff7ea1925feb904e50eead79c3f16b
laura <lauraparracosta11@gmail.com>
2024-11-10 21:22:28 +0100

    Define additional interfaces for video metadata #80

--- frontend/src/model/VideoPreviewData.tsx
+++ frontend/src/model/VideoPreviewData.tsx
@@ -1,6 +1,36 @@
 export interface VideoPreviewData {
+    meta: any;
     videoFileName: string;
     thumbnailFileName: string;
     title: string;
     username: string;
 }
+
+export interface Tag {
+    tagName: string;
+  }
+  
+export interface Category {
+    categoryName: string;
+  }
+  
+export interface Comment {
+    videoId: string;
+    username: string;
+    text: string;
+  }
+  
+export interface Video {
+    videoFileName: string;
+    title: string;
+    meta: {
+      tags: Tag[];
+      categories: Category[];
+      comments: Comment[];
+    };
+  }
+  
+export interface VideoDetailsProps {
+    video: Video;
+    onBack: () => void;
+  }
