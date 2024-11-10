c6a0d77a0571ea3254cb0f304208b70faf4cd190
laura <lauraparracosta11@gmail.com>
2024-11-10 18:58:10 +0100

    Apply Bootstrap classes and use relative units #76

--- frontend/src/components/VideoDetails.tsx
+++ frontend/src/components/VideoDetails.tsx
@@ -9,52 +9,30 @@ interface VideoDetailsProps {
   buttonColor?: string;
 }
 
-const VideoDetails: React.FC<VideoDetailsProps> = ({ video, onBack, buttonColor }) => {
-  const buttonStyle: React.CSSProperties = {
-    color: buttonColor || '#ffffff',
-    background: 'none',
-    border: 'none',
-    fontSize: '36px',
-    cursor: 'pointer',
-    position: 'absolute',
-    top: '20px',
-    left: '20px',
-    zIndex: 1
-  };
-
-  const containerStyle: React.CSSProperties = {
-    position: 'relative',
-    paddingTop: '70px'
-  };
-
-  const videoStyle: React.CSSProperties = {
-    width: '100%',
-    maxWidth: '1000px', // Asegura un tamaño máximo consistente para los videos
-    height: 'auto'
-  };
-
+const VideoDetails: React.FC<VideoDetailsProps> = ({ video, onBack }) => {
   return (
-    <div className="container" style={containerStyle}>
-      <div style={{ position: 'absolute', top: 0, left: 0 }}>
+    <div className="container pt-4"> {}
+      <div className="d-flex justify-content-start mb-2"> {}
         <button
-          className="btn btn-primary back-button"
+          className="btn btn-primary"
           onClick={onBack}
-          style={buttonStyle}
+          style={{ backgroundColor: 'transparent', color: '#ffffff' }}  
         >
           ←
         </button>
       </div>
-      <div className="row" style={{ position: 'relative' }}>
+      <div className="row mt-5"> {}
         <div className="col">
           <video
             data-testid="video-element"
             controls
             src={`${getEnv().MEDIA_BASE_URL}/${video.videoFileName}`}
-            style={videoStyle} // Aplicamos el estilo del video
+            className="w-100"
+            style={{ maxWidth: '90vw', height: 'auto' }} 
           />
         </div>
       </div>
-      <div className="details">
+      <div className="details mt-3">
         <h2>{video.title}</h2>
       </div>
     </div>
