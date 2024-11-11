2d26a28a3d3a373df3152e5386a8403af81e5fa8
Aitor Juanola <119131537+ajutra@users.noreply.github.com>
2024-11-11 13:26:30 +0100

    Implement header testing

---
+++ frontend/src/components/__tests__/Header.test.tsx
@@ -0,0 +1,28 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { BrowserRouter, useNavigate } from "react-router-dom";
+import Header from "../Header";
+import { AppRoutes } from "../../enums/AppRoutes";
+
+const mockNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockNavigate,
+}));
+
+describe("Header Component", () => {
+  test("renders the title and handles click", () => {
+    render(
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
+    );
+
+    const titleElement = screen.getByText("Protube");
+    expect(titleElement).toBeInTheDocument();
+
+    fireEvent.click(titleElement);
+    expect(mockNavigate).toHaveBeenCalledWith(AppRoutes.HOME);
+  });
+});
