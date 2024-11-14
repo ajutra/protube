9876c33b30beba0997d00023ad2f6c51c89af413
laura <lauraparracosta11@gmail.com>
2024-11-14 22:17:32 +0100

    Implement Tailwind CSS styling #89

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 import AppRouter from "./AppRouter.tsx";
+import "./output.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
