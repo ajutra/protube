f0cef3025446bb1e64c4f7aff3bb889c147ecf86
Aitor Juanola <119131537+ajutra@users.noreply.github.com>
2024-11-16 17:14:46 +0100

    Install tailwind and shadcn

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,52 @@
+import Header from "../components/Header";
+import "../components/styles/VideoCard.css";
+import VideoCard from "../components/VideoCard";
+import { VideoPreviewData } from "../model/VideoPreviewData";
+import { getEnv } from "../utils/Env";
+import { useEffect, useState } from "react";
+
+function Home() {
+  const [videos, setVideos] = useState<VideoPreviewData[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(getEnv().API_BASE_URL + "/videos")
+      .then((response) => response.json())
+      .then((data) => {
+        setVideos(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching videos: ", error);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="App p-5">
+      <Header />
+      <div className="container mt-5">
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : videos.length > 0 ? (
+          <div className="row">
+            {videos.map((video, index) => (
+              <div key={index} className="h-100 col-md-4 col-lg-3 mb-4">
+                <VideoCard
+                  videoFileName={video.videoFileName}
+                  thumbnailFileName={video.thumbnailFileName}
+                  title={video.title}
+                  username={video.username}
+                />
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p>No videos found</p>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
