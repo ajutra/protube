f31d05478d3f694163dcd4c8f4124d03ea2aec6a
laura <lauraparracosta11@gmail.com>
2024-11-16 13:21:23 +0100

    Fix unique comment keys #80

--- frontend/src/components/Comments.tsx
+++ frontend/src/components/Comments.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import { Comment } from  '../model/Comment';
+import React from "react";
+import { Comment } from "../model/Comment";
 
 const Comments: React.FC<{ comments: Comment[] }> = ({ comments }) => {
   return (
     <div className="comments mt-4">
-      <h4 className="text-secondary border-bottom pb-2 text-start">{comments.length} COMMENTS:</h4>
+      <h4 className="text-secondary border-bottom pb-2 text-start">
+        {comments.length} COMMENTS:
+      </h4>
       {comments.length > 0 ? (
-        comments.map((comment) => (
-          <div key={comment.videoId} className="comment mb-2 p-3 border rounded bg-secondary w-100">
-            <strong className="fs-5 text-start d-block">{comment.username}</strong>
+        comments.map((comment, index) => (
+          <div
+            key={index}
+            className="comment mb-2 p-3 border rounded bg-secondary w-100"
+          >
+            <strong className="fs-5 text-start d-block">
+              {comment.username}
+            </strong>
             <p className="mb-0 fs-6 text-start">{comment.text}</p>
           </div>
         ))
