5604e7596edb0337d53b4ed5ee90db5a4c56c798
Aitor Juanola <119131537+ajutra@users.noreply.github.com>
2024-11-16 17:51:29 +0100

    Apply theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import { AppRoutes } from './enums/AppRoutes'
 import Home from './pages/Home'
+import { ThemeProvider } from './components/themeProvider'
 
 const App: React.FC = () => {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path={AppRoutes.HOME} element={<Home />} />
-            </Routes>
+            <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+                <Routes>
+                    <Route path={AppRoutes.HOME} element={<Home />} />
+                </Routes>
+            </ThemeProvider>
         </BrowserRouter>
     )
 }
