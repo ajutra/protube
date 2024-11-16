213db628a2111e588e25ccc4250dc1e87a04ebff
Aitor Juanola <119131537+ajutra@users.noreply.github.com>
2024-11-16 16:59:08 +0100

    Remove bootstrap

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "bootstrap/dist/css/bootstrap.css";
 import AppRouter from "./AppRouter.tsx";
 
 createRoot(document.getElementById("root")!).render(
