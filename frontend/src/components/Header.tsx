00ce53a041e55b41d76c4420a9cc21807db6feaf
Aitor Juanola <119131537+ajutra@users.noreply.github.com>
2024-11-17 18:09:07 +0100

    Implement login form

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -6,7 +6,7 @@ import { LoginButton } from './LoginButton'
 
 const Header: React.FC = () => {
   return (
-    <header className="fixed flex w-screen items-center justify-between rounded-b-xl bg-accent p-4 text-foreground">
+    <header className="fixed flex w-screen items-center justify-between rounded-b-xl bg-background p-4 text-foreground">
       <Link to={AppRoutes.HOME} className="text-start text-4xl font-bold">
         Protube
       </Link>
--- frontend/src/components/LoginButton.tsx
+++ frontend/src/components/LoginButton.tsx
@@ -1,11 +1,33 @@
 import { CircleUserRound } from 'lucide-react'
 import { Button } from '@/components/ui/button'
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogTitle,
+  DialogDescription,
+} from '@/components/ui/dialog'
+import { LoginForm } from '@/components/LoginForm'
+import * as VisuallyHidden from '@radix-ui/react-visually-hidden'
 
 export function LoginButton() {
   return (
-    <Button variant="outline">
-      <CircleUserRound />
-      <span>Log In</span>
-    </Button>
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">
+          <CircleUserRound />
+          <span>Log In</span>
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <VisuallyHidden.Root>
+          <DialogTitle>Login</DialogTitle>
+          <DialogDescription>
+            Enter your username and password below to login to your account
+          </DialogDescription>
+        </VisuallyHidden.Root>
+        <LoginForm />
+      </DialogContent>
+    </Dialog>
   )
 }
