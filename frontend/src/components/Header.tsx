c8a7006c05ad564375c148753e231eeefe54e348
Aitor Juanola <119131537+ajutra@users.noreply.github.com>
2024-11-17 15:44:00 +0100

    Add profile logo

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { AppRoutes } from '../enums/AppRoutes'
+import ProfileLogo from '../assets/profileLogo.svg'
 
 const Header: React.FC = () => {
   return (
@@ -8,6 +9,11 @@ const Header: React.FC = () => {
       <Link to={AppRoutes.HOME} className="text-start text-4xl font-bold">
         Protube
       </Link>
+      <img
+        src={ProfileLogo}
+        alt="Profile Logo"
+        className="float-right h-10 w-10 stroke-primary-foreground"
+      />
     </header>
   )
 }
