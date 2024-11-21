d4d54f9f4383ddef01f9837d96c3f05684d7c959
laura <lauraparracosta11@gmail.com>
2024-11-22 00:21:12 +0100

    Modify Profile page #87

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,21 +1,26 @@
 import { Avatar, AvatarFallback } from '@/components/ui/avatar'
 import { useAuth } from '@/context/AuthContext'
+import ProfileTabs from '@/components/Tabs'
 
 function Profile() {
   const { username } = useAuth()
   const description = 'Welcome to your profile page'
-  return (
-    <div className="mt-6 flex flex-col items-center space-y-4 sm:flex-row sm:justify-center sm:space-x-4 sm:space-y-0">
-      <Avatar className="h-20 w-20">
-        <AvatarFallback className="bg-primary text-4xl font-bold">
-          {username?.charAt(0).toUpperCase()}
-        </AvatarFallback>
-      </Avatar>
 
-      <div className="space-y-2 text-center sm:text-left">
-        <h1 className="text-2xl font-bold">{username}</h1>
-        <p>{description}</p>
+  return (
+    <div className="container mx-auto mt-12 px-8">
+      <div className="mt-6 flex flex-col items-center space-y-4 sm:flex-row sm:justify-center sm:space-x-4 sm:space-y-0">
+        <Avatar className="h-20 w-20">
+          <AvatarFallback className="bg-primary text-4xl font-bold">
+            {username?.charAt(0).toUpperCase()}
+          </AvatarFallback>
+        </Avatar>
+
+        <div className="space-y-2 text-center sm:text-left">
+          <h1 className="text-2xl font-bold">{username}</h1>
+          <p>{description}</p>
+        </div>
       </div>
+      <ProfileTabs />
     </div>
   )
 }
