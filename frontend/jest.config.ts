60d5fefc9eb5fcb7d1237750e29c8783281237a9
Aitor Juanola <119131537+ajutra@users.noreply.github.com>
2024-11-21 16:23:53 +0100

    Update coverage thresholds

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -13,10 +13,10 @@ const config: Config = {
   extensionsToTreatAsEsm: ['.ts', '.tsx'],
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
+      branches: 95,
+      functions: 95,
+      lines: 95,
+      statements: 95,
     },
   },
   coveragePathIgnorePatterns: ['<rootDir>/src/hooks/use-toast.ts'],
