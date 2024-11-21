13ca634bccf7a882ef56b8c6cece85287081e9c7
Aitor Juanola <119131537+ajutra@users.noreply.github.com>
2024-11-21 15:56:30 +0100

    Remove unused field

--- backend/src/main/java/com/tecnocampus/LS2/protube_back/configuration/MvcConfig.java
+++ backend/src/main/java/com/tecnocampus/LS2/protube_back/configuration/MvcConfig.java
@@ -1,7 +1,5 @@
 package com.tecnocampus.LS2.protube_back.configuration;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -10,10 +8,6 @@ import org.springframework.web.servlet.config.annotation.*;
 @Configuration
 @EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer {
-
-    private static final Logger LOG =
-            LoggerFactory.getLogger(MvcConfig.class);
-
     @Autowired
     private Environment env;
 
