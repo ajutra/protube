cfee1e415342bd063ae3ea3a1134d69582d4284b
Aitor Juanola <119131537+ajutra@users.noreply.github.com>
2024-11-24 14:42:40 +0100

    Remove unused imports

--- backend/src/main/java/com/tecnocampus/LS2/protube_back/adapter/out/persistence/repository/VideoRepository.java
+++ backend/src/main/java/com/tecnocampus/LS2/protube_back/adapter/out/persistence/repository/VideoRepository.java
@@ -2,7 +2,6 @@ package com.tecnocampus.LS2.protube_back.adapter.out.persistence.repository;
 
 import com.tecnocampus.LS2.protube_back.adapter.out.persistence.jpaEntity.VideoJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
