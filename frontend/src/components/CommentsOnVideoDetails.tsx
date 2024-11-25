053b66295fe0b1897cb427e9aed149e868c062bc
nil roset <105341833+nilroset@users.noreply.github.com>
2024-11-25 15:20:15 +0100

    make that only users can comment

--- frontend/src/components/CommentsOnVideoDetails.tsx
+++ frontend/src/components/CommentsOnVideoDetails.tsx
@@ -12,15 +12,15 @@ interface CommentsOnVideoDetailsProps {
 }
 
 const CommentsOnVideoDetails: React.FC<CommentsOnVideoDetailsProps> = ({ comments, videoId }) => {
-  const username = useAuth().username;
-  const {handleDeletedComment} = useCommentsWithVideoTitle(username || '');
+  const { username, isLoggedIn } = useAuth();
+  const { handleDeletedComment } = useCommentsWithVideoTitle(username || '');
 
   return (
     <div className="mt-4 p-4">
       <h2 className="text-left text-2xl font-bold mb-6">
         {comments.length} Comments
       </h2>
-      <LeaveComment username={username || ''} videoId={videoId || ''} />
+      {isLoggedIn && <LeaveComment username={username || ''} videoId={videoId || ''} />}
       {comments.length > 0 ? (
         comments.map((comment, index) => (
           <React.Fragment key={index}>
