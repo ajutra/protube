3053ca00a6e573ca23e1888723759e23dfd0d067
laura <lauraparracosta11@gmail.com>
2024-11-29 18:20:55 +0100

    Refactor Video storage functionality #87

--- backend/src/main/java/com/tecnocampus/LS2/protube_back/port/in/useCase/StoreVideoUseCase.java
+++ backend/src/main/java/com/tecnocampus/LS2/protube_back/port/in/useCase/StoreVideoUseCase.java
@@ -1,11 +1,9 @@
 package com.tecnocampus.LS2.protube_back.port.in.useCase;
 
-
 import com.tecnocampus.LS2.protube_back.port.in.command.StoreVideoCommand;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-
 public interface StoreVideoUseCase {
     void storeVideo(StoreVideoCommand storeVideoCommand);
+    void storeVideoWithFiles(MultipartFile file, MultipartFile thumbnail, StoreVideoCommand storeVideoCommand);
 }
--- backend/src/main/java/com/tecnocampus/LS2/protube_back/port/in/useCase/UploadVideoUseCase.java
+++
@@ -1,8 +0,0 @@
-package com.tecnocampus.LS2.protube_back.port.in.useCase;
-
-import com.tecnocampus.LS2.protube_back.port.in.command.StoreVideoCommand;
-import org.springframework.web.multipart.MultipartFile;
-
-public interface UploadVideoUseCase {
-    void storeVideoWithFiles(MultipartFile file, MultipartFile thumbnail, StoreVideoCommand storeVideoCommand);
-}
