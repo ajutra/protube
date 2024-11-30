da3c5479522c42d076962a335dee4c3e28aa7549
nil roset <105341833+nilroset@users.noreply.github.com>
2024-11-30 17:15:49 +0100

    fix bug

--- frontend/src/components/Comments.tsx
+++ frontend/src/components/Comments.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Comment from './Comment';
 import { useAuth } from '@/context/AuthContext';
 import { LeaveComment } from './LeaveComment';
 import { cn } from '@/lib/utils';
 import { Comment as CommentType } from '@/model/Comment';
+import { getEnv } from '@/utils/Env';
 
 interface CommentsProps {
   comments: CommentType[];
@@ -14,6 +15,21 @@ interface CommentsProps {
 const Comments: React.FC<CommentsProps> = ({ comments, className, videoId }) => {
   const { username, isLoggedIn } = useAuth();
 
+  const fetchComments = async () => {
+    try {
+      const response = await fetch(`${getEnv().API_BASE_URL}/videos/${videoId}/comments`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch comments');
+      }
+      const data = await response.json();
+      setCommentList(data);
+    } catch (error) {
+      console.error('Error fetching comments:', error);
+    }
+  };
+  useEffect(() => {
+    fetchComments();
+  }, [videoId]);
   
   const sortedComments = [...comments].sort((a, b) => {
     if (a.username === username) return -1;
@@ -29,7 +45,7 @@ const Comments: React.FC<CommentsProps> = ({ comments, className, videoId }) => 
     );
   };
 
-  const handleNewComment = (newComment: { videoId: string; username: string; text: string }) => {
+  const handleNewComment = async (newComment: { videoId: string; username: string; text: string }) => {
     const commentWithId: CommentType = {
       ...newComment,
       commentId: new Date().toISOString(), 
@@ -42,6 +58,7 @@ const Comments: React.FC<CommentsProps> = ({ comments, className, videoId }) => 
         return 0;
       });
     });
+    await fetchComments();
   };
 
   return (
