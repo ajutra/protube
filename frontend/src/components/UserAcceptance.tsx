5819b76446862acd78fb10463046acb74d658718
Aitor Juanola <119131537+ajutra@users.noreply.github.com>
2024-12-01 18:15:35 +0100

    Add hover card to display user acceptance ratio

--- frontend/src/components/UserAcceptance.tsx
+++ frontend/src/components/UserAcceptance.tsx
@@ -1,6 +1,11 @@
 import { Progress } from '@/components/ui/progress'
 import { cn } from '@/lib/utils'
 import { useProgressColor } from '@/hooks/useProgressColor'
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from '@/components/ui/hover-card'
 
 interface UserAcceptanceProps {
   likes: number
@@ -13,12 +18,19 @@ const UserAcceptance = ({ likes, dislikes }: UserAcceptanceProps) => {
   const { getColor } = useProgressColor()
 
   return (
-    <div className="flex max-h-8 w-full items-center space-x-2">
-      <Progress value={acceptance} className="w-32" />
-      <span className={cn(`font-bold text-${getColor(acceptance)}`)}>
-        {acceptance.toFixed(0)}%
-      </span>
-    </div>
+    <HoverCard>
+      <HoverCardTrigger asChild>
+        <div className="flex max-h-8 w-full items-center space-x-2">
+          <Progress value={acceptance} className="w-32" />
+          <span className={cn(`font-bold text-${getColor(acceptance)}`)}>
+            {acceptance.toFixed(0)}%
+          </span>
+        </div>
+      </HoverCardTrigger>
+      <HoverCardContent>
+        <span className="text-sm font-normal">User acceptance ratio.</span>
+      </HoverCardContent>
+    </HoverCard>
   )
 }
 
