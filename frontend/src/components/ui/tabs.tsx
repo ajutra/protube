5d942528bafa420d18d01d5ea05a6a53241f930f
nil roset <105341833+nilroset@users.noreply.github.com>
2024-12-01 12:17:16 +0100

    change tab color

--- frontend/src/components/ui/tabs.tsx
+++ frontend/src/components/ui/tabs.tsx
@@ -27,7 +27,7 @@ const TabsTrigger = React.forwardRef<
     ref={ref}
     className={cn(
       'inline-flex items-center justify-center whitespace-nowrap px-3 py-1 text-sm font-medium transition-all focus-visible:outline-none focus-visible:ring-0 disabled:pointer-events-none disabled:opacity-50 border-b-2',
-      'data-[state=active]:border-b-primary data-[state=active]:text-primary data-[state=inactive]:border-b-primary/50 data-[state=inactive]:text-primary/50',
+      'data-[state=active]:border-b-foreground data-[state=active]:text-foreground data-[state=inactive]:border-b-foreground/50 data-[state=inactive]:text-foreground/50',
       className
     )}
     {...props}
