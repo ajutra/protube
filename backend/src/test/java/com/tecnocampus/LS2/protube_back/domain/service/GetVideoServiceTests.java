a8ba8c88a2e499ec6f7eaac6bae4d8b2dea3bccd
Aitor Juanola <119131537+ajutra@users.noreply.github.com>
2024-12-03 21:39:05 +0100

    Merge GetVideoByIdService with GetVideoService

--- backend/src/test/java/com/tecnocampus/LS2/protube_back/domain/service/GetVideoServiceTests.java
+++ backend/src/test/java/com/tecnocampus/LS2/protube_back/domain/service/GetVideoServiceTests.java
@@ -1,7 +1,10 @@
 package com.tecnocampus.LS2.protube_back.domain.service;
 
 import com.tecnocampus.LS2.protube_back.TestObjectFactory;
+import com.tecnocampus.LS2.protube_back.domain.model.Field;
+import com.tecnocampus.LS2.protube_back.domain.model.PlayerPageVideo;
 import com.tecnocampus.LS2.protube_back.domain.model.Video;
+import com.tecnocampus.LS2.protube_back.port.in.command.GetVideoCommand;
 import com.tecnocampus.LS2.protube_back.port.out.GetVideoPort;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,6 +13,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.NoSuchElementException;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -45,5 +49,26 @@ public class GetVideoServiceTests {
         when(getVideoPort.getVideoByTitleAndUsername(any(), any())).thenThrow(NoSuchElementException.class);
 
         assertThrows(NoSuchElementException.class, () -> getVideoService.getVideoByTitleAndUsername("1", "1"));
+    }
+
+    @Test
+    void getVideoByIdReturnsVideo() {
+        String videoId = "1";
+        PlayerPageVideo playerPageVideo = TestObjectFactory.createDummyPlayerPageVideo(videoId);
+        when(getVideoPort.getVideoWithFieldsById(videoId, Set.of(Field.CATEGORIES, Field.TAGS, Field.COMMENTS)))
+                .thenReturn(playerPageVideo);
+
+        GetVideoCommand result = getVideoService.getVideoById(videoId);
+
+        assertEquals(GetVideoCommand.from(playerPageVideo.video(), playerPageVideo.categories(), playerPageVideo.tags(), playerPageVideo.comments()), result);
+    }
+
+    @Test
+    void getVideoByIdThrowsExceptionWhenVideoNotFound() {
+        String videoId = "999";
+        when(getVideoPort.getVideoWithFieldsById(videoId, Set.of(Field.CATEGORIES, Field.TAGS, Field.COMMENTS)))
+                .thenThrow(new NoSuchElementException("Video not found"));
+
+        assertThrows(NoSuchElementException.class, () -> getVideoService.getVideoById(videoId));
     }
 }
