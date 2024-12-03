270353afe5a18c06af8134c9d338c3b1f76c38db
laura <lauraparracosta11@gmail.com>
2024-12-03 20:56:48 +0100

    Add logo to Header.tsx #115

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -5,14 +5,27 @@ import { ModeToggle } from './ModeToggle'
 import { LoginButton } from './LoginButton'
 import { useAuth } from '@/context/AuthContext'
 import { ProfileButton } from './ProfileButton'
+import logoLight from '../assets/logoLight.png'
+import logoDark from '../assets/logoDark.png'
 
 const Header: React.FC = () => {
   const { isLoggedIn } = useAuth()
 
   return (
-    <header className="fixed flex w-screen items-center justify-between rounded-b-xl bg-background p-8 text-foreground z-50">
-      <Link to={AppRoutes.HOME} className="text-start text-4xl font-bold">
-        Protube
+    <header className="fixed flex w-screen items-center justify-between rounded-b-xl bg-background p-8 text-foreground">
+      <Link to={AppRoutes.HOME} className="flex items-center text-start">
+        <img
+          src={logoLight}
+          alt="Logo Light"
+          className="hidden h-12 dark:block md:h-20"
+          style={{ maxHeight: '4rem', height: 'auto' }}
+        />
+        <img
+          src={logoDark}
+          alt="Logo Dark"
+          className="block h-12 dark:hidden md:h-20"
+          style={{ maxHeight: '4rem', height: 'auto' }}
+        />
       </Link>
       <div className="float-right flex items-center space-x-5">
         <ModeToggle />
