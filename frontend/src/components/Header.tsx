fb6cda01e4314ab4ac3b8f03907a75194a37baab
Aitor Juanola <119131537+ajutra@users.noreply.github.com>
2024-12-04 01:43:44 +0100

    Add SearchBar component into the Header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,55 +1,28 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
-import { AppRoutes } from '../enums/AppRoutes'
-import { ModeToggle } from './ModeToggle'
-import { LoginButton } from './LoginButton'
 import { useAuth } from '@/context/AuthContext'
-import { ProfileButton } from './ProfileButton'
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-} from '@/components/ui/command'
+import { AppRoutes } from '@/enums/AppRoutes'
+import { ModeToggle } from '@/components/ModeToggle'
+import { ProfileButton } from '@/components/ProfileButton'
+import { LoginButton } from '@/components/LoginButton'
+import SearchBar from '@/components/SearchBar'
 
 const Header: React.FC = () => {
   const { isLoggedIn } = useAuth()
-  const [isSearching, setIsSearching] = useState(false)
 
   return (
-    <header className="fixed z-50 flex w-screen items-center justify-between rounded-b-xl bg-background p-8 text-foreground">
+    <div className="fixed z-50 flex w-screen items-center justify-between rounded-b-xl bg-background p-8 text-foreground">
       <Link to={AppRoutes.HOME} className="text-start text-4xl font-bold">
         Protube
       </Link>
-      <div className="relative">
-        <Command className="rounded-lg border focus-within:border-primary md:min-w-[450px]">
-          <CommandInput
-            placeholder="Search"
-            onFocus={() => setIsSearching(true)}
-            onBlur={() => setIsSearching(false)}
-          />
-          {isSearching && (
-            <CommandList className="absolute left-0 right-0 top-full mt-1 overflow-y-auto rounded-xl bg-background">
-              <CommandEmpty>No results found.</CommandEmpty>
-              <CommandGroup heading="Suggestions">
-                <CommandItem>
-                  <span>Calendar</span>
-                </CommandItem>
-                <CommandItem>
-                  <span>Search Emoji</span>
-                </CommandItem>
-              </CommandGroup>
-            </CommandList>
-          )}
-        </Command>
+      <div className="relative w-2/4">
+        <SearchBar />
       </div>
       <div className="float-right flex items-center space-x-5">
         <ModeToggle />
         {isLoggedIn ? <ProfileButton /> : <LoginButton />}
       </div>
-    </header>
+    </div>
   )
 }
 
---
+++ frontend/src/components/SearchBar.tsx
@@ -0,0 +1,137 @@
+import React, { useState, useRef, useEffect } from 'react'
+import { Search, X } from 'lucide-react'
+import { getEnv } from '@/utils/Env'
+import { AppRoutes } from '@/enums/AppRoutes'
+import { Link } from 'react-router-dom'
+import { Button } from '@/components/ui/button'
+
+interface SearchResult {
+  id: string
+  title: string
+}
+
+const SearchBar: React.FC = () => {
+  const [isSearching, setIsSearching] = useState(false)
+  const [searchResults, setSearchResults] = useState<SearchResult[]>([])
+  const inputRef = useRef<HTMLInputElement>(null)
+  const searchTimeoutRef = useRef<NodeJS.Timeout | null>(null)
+  const searchResultsRef = useRef<HTMLDivElement>(null)
+
+  const handleSearch = async (query: string) => {
+    if (!query.trim()) {
+      setSearchResults([])
+      return
+    }
+    const response = await fetch(
+      getEnv().API_BASE_URL + `/videos/search/${query}`
+    )
+    const results: SearchResult[] = await response.json()
+    setSearchResults(results)
+  }
+
+  useEffect(() => {
+    if (isSearching) {
+      const handleInputChange = () => {
+        if (inputRef.current) {
+          const query = inputRef.current.value
+          if (searchTimeoutRef.current) {
+            clearTimeout(searchTimeoutRef.current)
+          }
+          searchTimeoutRef.current = setTimeout(() => {
+            handleSearch(query)
+          }, 200)
+        }
+      }
+
+      const inputElement = inputRef.current
+      inputElement?.addEventListener('input', handleInputChange)
+
+      return () => {
+        inputElement?.removeEventListener('input', handleInputChange)
+        if (searchTimeoutRef.current) {
+          clearTimeout(searchTimeoutRef.current)
+        }
+      }
+    }
+  }, [isSearching])
+
+  const handleMouseDown = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (
+      searchResultsRef.current &&
+      searchResultsRef.current.contains(event.target as Node)
+    ) {
+      event.preventDefault()
+    }
+  }
+
+  const clearInput = () => {
+    if (inputRef.current) {
+      inputRef.current.value = ''
+    }
+    setIsSearching(false)
+  }
+
+  const handleLinkClick = () => {
+    setIsSearching(false)
+    if (inputRef.current) {
+      inputRef.current.blur()
+    }
+  }
+
+  return (
+    <div className="relative w-full rounded-full">
+      <div className={`absolute left-0 top-0 flex h-full items-center pl-5`}>
+        {isSearching && <Search className="h-5 w-5" />}
+      </div>
+      <input
+        ref={inputRef}
+        placeholder="Search"
+        onFocus={() => setIsSearching(true)}
+        onBlur={(e) => {
+          if (!searchResultsRef.current?.contains(e.relatedTarget)) {
+            setIsSearching(false)
+          }
+        }}
+        className={`w-full rounded-full border bg-background px-3 py-2 text-sm placeholder:text-muted-foreground focus:border-primary focus:outline-none ${isSearching ? 'pl-14' : 'pl-5'}`}
+      />
+      <Button
+        onClick={clearInput}
+        variant="ghost"
+        size="icon"
+        className="absolute right-0 top-0 mr-2 rounded-full text-lg"
+      >
+        <X />
+      </Button>
+      {isSearching && (
+        <div
+          ref={searchResultsRef}
+          className="absolute left-0 right-0 top-full z-50 mt-2 max-h-96 w-full overflow-y-auto rounded-xl bg-secondary"
+          onMouseDown={handleMouseDown}
+        >
+          {searchResults.length === 0 ? (
+            <div className="px-5 py-4 text-sm font-bold text-secondary-foreground">
+              No results found.
+            </div>
+          ) : (
+            <div className="py-3">
+              {searchResults.map((result) => (
+                <div key={result.id}>
+                  <Link
+                    to={AppRoutes.VIDEO_DETAILS + '?id=' + result.id}
+                    className="z-50 flex flex-row items-center gap-4 px-5 py-2 text-sm text-secondary-foreground hover:bg-background"
+                    onClick={handleLinkClick}
+                  >
+                    <Search className="h-5 w-5" />
+                    {result.title}
+                  </Link>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default SearchBar
