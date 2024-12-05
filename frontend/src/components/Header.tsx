9d54f1d9c01c4f5f908b445aa6fd3489cb7d8491
Aitor Juanola <119131537+ajutra@users.noreply.github.com>
2024-12-05 18:00:40 +0100

    Refactor Header to improve layout

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -5,33 +5,29 @@ import { ModeToggle } from './ModeToggle'
 import { LoginButton } from './LoginButton'
 import { useAuth } from '@/context/AuthContext'
 import { ProfileButton } from './ProfileButton'
-import logoLight from '../assets/logoLight.png'
-import logoDark from '../assets/logoDark.png'
+import logoLightMode from '@/assets/logoLightMode.png'
+import logoDarkMode from '@/assets/logoDarkMode.png'
 
 const Header: React.FC = () => {
   const { isLoggedIn } = useAuth()
 
   return (
-    <header className="fixed z-50 w-full bg-background p-4 text-foreground">
-      <div className="flex items-center justify-between pl-4 pr-4">
-        <Link to={AppRoutes.HOME} className="flex items-center text-start">
-          <img
-            src={logoLight}
-            alt="Logo Light"
-            className="hidden h-16 dark:block md:h-24"
-            style={{ height: 'auto', maxHeight: '3.5rem' }}
-          />
-          <img
-            src={logoDark}
-            alt="Logo Dark"
-            className="block h-16 dark:hidden md:h-24"
-            style={{ height: 'auto', maxHeight: '3.5rem' }}
-          />
-        </Link>
-        <div className="flex items-center space-x-5">
-          <ModeToggle />
-          {isLoggedIn ? <ProfileButton /> : <LoginButton />}
-        </div>
+    <header className="fixed z-50 flex w-screen items-center justify-between rounded-b-xl bg-background pb-2 pl-6 pr-8 pt-6 text-foreground">
+      <Link to={AppRoutes.HOME} className="flex items-center">
+        <img
+          src={logoDarkMode}
+          alt="Protube Logo"
+          className="hidden w-40 dark:block"
+        />
+        <img
+          src={logoLightMode}
+          alt="Protube Logo"
+          className="w-40 dark:hidden"
+        />
+      </Link>
+      <div className="flex items-center space-x-5">
+        <ModeToggle />
+        {isLoggedIn ? <ProfileButton /> : <LoginButton />}
       </div>
     </header>
   )
